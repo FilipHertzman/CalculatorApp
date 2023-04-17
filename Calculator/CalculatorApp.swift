8ec8c0a222fcf8eda62999d40d1a75c616d1848e
filiphertzman <filip.hertzman@gmail.com>
2023-04-17 20:26:39 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Filip Hertzman on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
